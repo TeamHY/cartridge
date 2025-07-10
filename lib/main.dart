70748cd50c4105a4042d9345035e1459a1d86469
Pdom <pdom0327@gmail.com>
2025-07-10 23:56:04 +0900

    Release 4.13.3

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:pub_semver/pub_semver.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:window_manager/window_manager.dart';
 
-final currentVersion = Version.parse('4.13.2');
+final currentVersion = Version.parse('4.13.3');
 
 void main() async {
   await dotenv.load(fileName: '.env');
